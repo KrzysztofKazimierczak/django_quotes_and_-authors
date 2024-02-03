cf29bab2f005bd79f6e4a1356cb86f7ec65bb279
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-03 22:02:01 +0100

    add add_quote mechanic, tag fix

--- quotesapp/forms.py
+++ quotesapp/forms.py
@@ -1,10 +1,20 @@
 from django.forms import ModelForm, CharField, TextInput
-from .models import Tag
+from .models import Tag, Quote
 
 class TagForm(ModelForm):
 
-    name = CharField(min_length=3, max_length=20, required=True, widget=TextInput())
+    tag = CharField(min_length=3, max_length=20, required=True, widget=TextInput())
 
     class Meta:
         model = Tag
         fields = ['tag']
+
+class QuoteForm(ModelForm):
+
+    author = CharField(min_length=5, max_length=50, required=True, widget=TextInput())
+    content = CharField(min_length=3, max_length=500, required=True, widget=TextInput())
+
+    class Meta:
+        model = Quote
+        fields = ['author', 'content']
+        exclude = ['tags']
--- quotesapp/urls.py
+++ quotesapp/urls.py
@@ -5,5 +5,6 @@ app_name = 'quotesapp'
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('add_quote/', views.add_quote, name='quote'),
     path('tag/', views.tag, name='tag'),
 ]
