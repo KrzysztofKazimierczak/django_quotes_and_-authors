1699abdad4132cbd43aed369485f72772a76870b
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-04 00:23:43 +0100

    fast fix

--- quotesapp/templatetags/extract_tags.py
+++ quotesapp/templatetags/extract_tags.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 def tags(quote_tags):
-    return ', '.join([str(tag) for tag in quote_tags.all()])
+    return ', '.join([str(name) for name in quote_tags.all()])
 
 register.filter('tags', tags)
