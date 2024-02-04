eb3806de14c11729ce28da83e52dddf9e62810cd
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-04 16:38:42 +0100

    add usersapp

--- quotesapp/urls.py
+++ quotesapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+
 
 app_name = 'quotesapp'
 
@@ -11,4 +14,11 @@ urlpatterns = [
     path('add_quote/', views.add_quote, name='add_quote'),
     path('add_tag/', views.add_tag, name='add_tag'),
     path('delete/<int:quote_id>', views.delete_quote, name='delete'),
+]
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include('quotesapp.urls')),
+    path('users/', include('users.urls')),
 ]
