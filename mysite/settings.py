172a6b809bfbe0f59778fefd61be173ada8dd0bc
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-04 17:27:18 +0100

    implement user signup

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quotesapp'
-    'users'
+    'quotesapp',
+    'users',
 ]
 
 MIDDLEWARE = [
