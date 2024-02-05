fd73e926a5ab97cafbb6df543bd86db64024d66d
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-05 18:09:39 +0100

    Implement scrapping option

--- scrap_with_bs.py
+++ scrap_with_bs.py
@@ -3,31 +3,30 @@ from bs4 import BeautifulSoup
 import json
 
 
-def find_data(url, span_to_srap):
+def find_data(url, data_to_scrap):
     data_list = []
-    start_url = url
-    
+    data_to_scrap = data_to_scrap.lower()
+   
     while url:
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = BeautifulSoup(response.text, 'html.parser')
     
-        for span_to_srap in soup.find_all('div', class_='quote'):
-            scrapped_data = span_to_srap.find('span', class_=span_to_srap).text
+        for span_to_scrap in soup.find_all('div', class_='record'):
+            scrapped_data = span_to_scrap.find('span', class_=data_to_scrap).get_text(strip=True)
+            print(scrapped_data)
 
-            if not any(item["quote"] == scrapped_data for item in data_list):
+            if not any(item[data_to_scrap] == scrapped_data for item in data_list):
                 data_list.append(
                     {
-                        span_to_srap: scrapped_data
+                        data_to_scrap: scrapped_data
                     })
                            
-
+        break
     return data_list
-"""
-    with open('quotes.json', 'w', encoding='utf-8') as quotes_file:
-        json.dump(quotes_list, quotes_file, indent=2, ensure_ascii=False)
 
-    with open('authors.json', 'w', encoding='utf-8') as authors_file:
-        json.dump(authors_list, authors_file, indent=2, ensure_ascii=False)"""
+
 
 if __name__ == "__main__":
-    pass
+    url="http://localhost:8000/"
+    print(find_data(url,"author"))
+    
