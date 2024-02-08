fedd50a364350a7423a8c04bacfa25c3a83e762a
KrzysztofKazimierczak <53145151+KrzysztofKazimierczak@users.noreply.github.com>
2024-02-08 22:56:34 +0100

    add feateure for tags and paginator

--- quotesapp/views.py
+++ quotesapp/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import TagForm, QuoteForm, AuthorForm, ScrapperForm
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import TagForm, QuoteForm, AuthorForm
 from .models import Tag, Quote, Author, ScrapData
 from scrap_with_bs import find_data
 import subprocess, os, json
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def main(request):
-    quotes = Quote.objects.select_related('author').all()
-    return render(request, 'quotesapp/index.html', {"quotes": quotes})
+    quotes_list = Quote.objects.select_related('author').all()
+    paginator = Paginator(quotes_list, 2)
+    page_number = request.GET.get('page')
+    try:
+        quotes = paginator.page(page_number)
+    except PageNotAnInteger:
+        quotes = paginator.page(1)
+    except EmptyPage:
+        quotes = paginator.page(paginator.num_pages)
+
+    top_tags = Tag.objects.annotate(num_quotes=Count('quote')).filter(num_quotes__gt=0).order_by('-num_quotes')[:10]
+    return render(request, 'quotesapp/index.html', {'quotes': quotes, 'top_tags': top_tags, 'page_obj': quotes})
+
 
 
 
@@ -65,20 +79,25 @@ def delete_quote(request, quote_id):
     Quote.objects.get(pk=quote_id).delete()
     return redirect(to='quotesapp:main')
 
-# Beautifoul soup scraper
+def quotes_by_tag(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    quotes = Quote.objects.filter(tags=tag)
+    return render(request, 'quotesapp/quotes_by_tag.html', {'tag': tag, 'quotes': quotes})
 
-"""
-def scraper(request, option):
-    url = "http://localhost:8000/"
-    data = find_data(url, option)
+def top_ten_tags(request):
+    quotes = Quote.objects.all()
+    tag_counts = Tag.objects.annotate(num_quotes=Count('quote')).filter(num_quotes__gt=0).order_by('-num_quotes')[:10]
+    
+    context = {
+        'quotes': quotes,
+        'top_tags': tag_counts,
+    }
+    return render(request, 'quotesapp/top_ten_tags.html', context)
 
-    scrap_data_object = ScrapData(choice=option, dictionary=data)
-    scrap_data_object.save()
 
-    return render(request, 'quotesapp/scraped_data.html', {'scraped_data': data})
-"""
 
 # Scrapy scraper
+
 def scraper(request, option):
     django_project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     scrapy_project_dir = os.path.join(django_project_dir, 'scrapmyside/scrapmyside')
@@ -98,3 +117,16 @@ def scraper(request, option):
     os.chdir(django_project_dir)
 
     return render(request, 'quotesapp/scraped_data.html', {'scraped_data': data})
+
+# Beautifoul soup scraper
+
+"""
+def scraper(request, option):
+    url = "http://localhost:8000/"
+    data = find_data(url, option)
+
+    scrap_data_object = ScrapData(choice=option, dictionary=data)
+    scrap_data_object.save()
+
+    return render(request, 'quotesapp/scraped_data.html', {'scraped_data': data})
+"""
